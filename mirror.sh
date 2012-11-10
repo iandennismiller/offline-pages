d696f1f2f94baa00d0f22f39499fe5f943bc95db
Ian Dennis Miller <ian@iandennismiller.com>
2012-11-10 16:30:05 -0500

    making all URIs self-referential within the archive

--- mirror.sh
+++ mirror.sh
@@ -2,7 +2,7 @@
 
 UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) \
 Chrome/23.0.1271.6 Safari/537.11"
-FIRST=$(head -n1 $1)
+FIRST=$(head -n1 $1 | sed 's/http:\/\//\//')
 
 mkdir -p www
 cat > www/index.html <<EOF 
@@ -12,4 +12,9 @@ cat > www/index.html <<EOF
 </head>
 </html>
 EOF
-wget -v -H -N -U "$UA" -k -p --warc-file=mirror -i "$1" --random-wait --wait=3 -P=www
+wget -v --span-hosts --timestamping --convert-links --page-requisites \
+    --random-wait --wait=3 --directory-prefix="www" --user-agent="$UA" --input-file="$1"
+
+LC_CTYPE='C'
+grep -ril 'http://' www | grep -v jpg |grep -v gif | grep -v png \
+    | xargs sed -i '' 's/http:\/\//\//g'
--- referential.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-LC_CTYPE='C'
-grep -ril 'http://' www | grep -v jpg |grep -v gif | grep -v png \
-    | xargs sed -i '' 's/http:\/\//\//g'
